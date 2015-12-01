2a9c4d56a20d72e5aae0b48a32f8a60b3d955f6b
Chun-Tie,Lin <style831211@gmail.com>
2015-12-02 01:01:12 +0800

    initial commit

---
+++ src/api/routes/articles.js
@@ -0,0 +1,35 @@
+import express from 'express';
+var router = express.Router();
+
+var articles = [];
+
+router.get('/', function (req, res) {
+  res.json(articles);
+});
+
+router.post('/', function (req, res) {
+  if (!req.body.title && !req.body.content && !req.body.weight && !req.body.reps) {
+    res.status(400);
+    return res.json({
+      message: 'Parameter Error'
+    });
+  }
+
+  var articleId = Math.random();
+
+  articles.push({
+    id: articleId,
+    title: req.body.title,
+    content: req.body.content,
+    event: req.body.event,
+    weight: req.body.weight,
+    reps: req.body.reps
+  });
+
+  res.status(201);
+  res.json({
+    id: articleId
+  });
+});
+
+module.exports = router;
